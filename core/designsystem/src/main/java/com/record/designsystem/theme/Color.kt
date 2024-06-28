4ea65b00501498b9b14d7081388ba58fe07c8507
Sangwook123 <operawook@catholic.ac.kr>
2024-06-28 19:39:28 +0900

    [feat] #1 core-designsystem module

---
+++ core/designsystem/src/main/java/com/record/designsystem/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.record.designsystem.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Gray Scale
+val Gray01 = Color(0xFFE2E5EC)
+val Gray03 = Color(0xFFA5A9B4)
+val Gray04 = Color(0xFF70737D)
+val Gray08 = Color(0xFF1B2129)
+val Gray09 = Color(0xFF181B1F)
+val Background = Color(0xFF04070E)
+
+// Main
+val Main = Color(0xFFBEC7F5)
+
+// Alert
+val Alert = Color(0xFFF56A6F)

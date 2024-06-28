e0106be8fab88d6813392fb0d9d89bf3b0c0f5b1
Sangwook123 <operawook@catholic.ac.kr>
2024-06-28 19:37:38 +0900

    [chore] #1 app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,32 +1,15 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.recordy.android.application)
     alias(libs.plugins.kotlin.android)
 }
 
 android {
     namespace = "com.record.recordy"
-    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.record.recordy"
-        minSdk = 28
-        targetSdk = 34
-        versionCode = 0
-        versionName = "1.0.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
-        }
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
         }
     }
     compileOptions {
@@ -50,20 +33,9 @@ android {
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.ui)
-    implementation(libs.ui.graphics)
-    implementation(libs.ui.tooling.preview)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.junit)
-    androidTestImplementation(libs.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.ui.test.junit4)
-    debugImplementation(libs.ui.tooling)
-    debugImplementation(libs.ui.test.manifest)
+    implementation(projects.core.common)
+    implementation(projects.core.designsystem)
+    implementation(projects.core.buildConfig)
+    implementation(projects.core.datastore)
+    implementation(projects.core.network)
 }

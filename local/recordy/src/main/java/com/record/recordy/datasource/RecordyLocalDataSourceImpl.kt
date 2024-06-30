6feeca56ab87d46165d2d28034e8452a54bb98eb
Sangwook123 <operawook@catholic.ac.kr>
2024-06-30 20:44:33 +0900

    [feat] #1 local example

---
+++ local/recordy/src/main/java/com/record/recordy/datasource/RecordyLocalDataSourceImpl.kt
@@ -0,0 +1,24 @@
+package com.record.recordy.datasource
+
+import android.util.Log
+import androidx.datastore.core.DataStore
+import com.record.recordy.source.local.RecordyLocalDataSource
+import org.sopt.datastore.RecordyLocalData
+import java.io.IOException
+import javax.inject.Inject
+
+class RecordyLocalDataSourceImpl @Inject constructor(
+    private val userPreferences: DataStore<RecordyLocalData>
+) : RecordyLocalDataSource {
+    override val recordyLocalData = userPreferences.data
+
+    override suspend fun setRecordyLocalData(recordyData: RecordyLocalData) {
+        try {
+            userPreferences.updateData {
+                it.copy(recordyData.a, recordyData.b)
+            }
+        } catch (ioException: IOException) {
+            Log.e("exception", "ioException")
+        }
+    }
+}

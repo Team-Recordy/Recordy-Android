5793818d6a47f26995688a9569dc99f5d1d30676
Sangwook123 <operawook@catholic.ac.kr>
2024-06-30 20:47:49 +0900

    [feat] #1 designsystem

--- core/designsystem/src/main/java/com/record/designsystem/theme/Color.kt
+++ core/designsystem/src/main/java/com/record/designsystem/theme/Color.kt
@@ -6,12 +6,18 @@ import androidx.compose.ui.graphics.Color
 val Gray01 = Color(0xFFE2E5EC)
 val Gray03 = Color(0xFFA5A9B4)
 val Gray04 = Color(0xFF70737D)
+val Gray05 = Color(0xFF41434A)
 val Gray08 = Color(0xFF1B2129)
-val Gray09 = Color(0xFF181B1F)
+val Gray09 = Color(0xFF12161C)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
 val Background = Color(0xFF04070E)
 
-// Main
-val Main = Color(0xFFBEC7F5)
+// Recordy
+val Main = Color(0xFF9BABFB)
+val Sub01 = Color(0xFF3F4054)
+val Sub02 = Color(0xFF21263C)
 
-// Alert
+// System
 val Alert = Color(0xFFF56A6F)
+val Kakao = Color(0xFFFEE500)

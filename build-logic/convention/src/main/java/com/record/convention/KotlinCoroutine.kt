46a11a47a5889b7bc18045325ebaa9aae843d92a
Sangwook123 <operawook@catholic.ac.kr>
2024-07-01 02:45:32 +0900

    [chore] #1 buildlogic

--- build-logic/convention/src/main/java/com/record/convention/KotlinCoroutine.kt
+++ build-logic/convention/src/main/java/com/record/convention/KotlinCoroutine.kt
@@ -1,8 +1,8 @@
 package com.record.convention
 
 import com.android.build.api.dsl.CommonExtension
-import com.record.convention.extension.debugImplementation
 import com.record.convention.extension.getBundle
+import com.record.convention.extension.implementation
 import com.record.convention.extension.libs
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.dependencies
@@ -12,7 +12,7 @@ internal fun Project.configureKotlinCoroutine(
 ) {
     commonExtension.apply {
         dependencies {
-            debugImplementation(libs.getBundle("coroutine"))
+            implementation(libs.getBundle("coroutine"))
         }
     }
 }

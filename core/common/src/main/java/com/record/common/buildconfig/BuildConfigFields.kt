0407d4655fe9203447fd336b21ca98028e5b5a99
김명석 <75840431+cacaocoffee@users.noreply.github.com>
2024-07-01 23:52:24 +0900

    [feat] #4 kakao hash key

--- core/common/src/main/java/com/record/common/buildconfig/BuildConfigFields.kt
+++ core/common/src/main/java/com/record/common/buildconfig/BuildConfigFields.kt
@@ -2,6 +2,6 @@ package com.record.common.buildconfig
 
 data class BuildConfigFields(
     val baseUrl: String,
-    val kakaoNativeKey: String,
+     val kakaoNativeKey: String,
     val isDebug: Boolean,
 )

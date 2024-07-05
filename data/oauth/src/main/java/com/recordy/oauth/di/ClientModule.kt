7fcf6944bfd29e6701ed623586fec5ff19b2ef60
김명석 <twinaudtjr@naver.com>
2024-07-05 22:28:45 +0900

    [feat] #4 ClientModule

---
+++ data/oauth/src/main/java/com/recordy/oauth/di/ClientModule.kt
@@ -0,0 +1,17 @@
+package com.recordy.oauth.di
+
+import com.kakao.sdk.user.UserApiClient
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ClientModule {
+    @Provides
+    @Singleton
+    fun provideKakaoClient(): UserApiClient = UserApiClient.instance
+}

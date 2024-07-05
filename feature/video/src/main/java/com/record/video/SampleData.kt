17c6bcb52158796cd3c230f23593c16fba293392
Sangwook123 <operawook@catholic.ac.kr>
2024-07-05 20:07:40 +0900

    [add] #47 example data

---
+++ feature/video/src/main/java/com/record/video/SampleData.kt
@@ -0,0 +1,77 @@
+package com.record.video
+
+object SampleData {
+    private val video1 = VideoData(
+        id = "1",
+        videoUri = "https://assets.mixkit.co/videos/preview/mixkit-tree-with-yellow-flowers-1173-large.mp4",
+        previewUri = "https://mixkit.imgix.net/videos/preview/mixkit-tree-with-yellow-flowers-1173-0.jpg",
+    )
+
+    private val video2 = VideoData(
+        id = "2",
+        videoUri = "https://assets.mixkit.co/videos/preview/mixkit-palm-tree-in-front-of-the-sun-1191-large.mp4",
+        previewUri = "https://mixkit.imgix.net/videos/preview/mixkit-palm-tree-in-front-of-the-sun-1191-0.jpg",
+    )
+
+    private val video3 = VideoData(
+        id = "3",
+        videoUri = "https://assets.mixkit.co/videos/preview/mixkit-red-frog-on-a-log-1487-large.mp4",
+        previewUri = "https://mixkit.imgix.net/videos/preview/mixkit-red-frog-on-a-log-1487-0.jpg",
+    )
+
+    private val video4 = VideoData(
+        id = "4",
+        videoUri = "https://assets.mixkit.co/videos/preview/mixkit-tree-with-yellow-flowers-1173-large.mp4",
+        previewUri = "https://mixkit.imgix.net/videos/preview/mixkit-tree-with-yellow-flowers-1173-0.jpg",
+    )
+
+    private val video5 = VideoData(
+        id = "5",
+        videoUri = "https://assets.mixkit.co/videos/preview/mixkit-palm-tree-in-front-of-the-sun-1191-large.mp4",
+        previewUri = "https://mixkit.imgix.net/videos/preview/mixkit-palm-tree-in-front-of-the-sun-1191-0.jpg",
+    )
+
+    private val video6 = VideoData(
+        id = "6",
+        videoUri = "https://assets.mixkit.co/videos/preview/mixkit-red-frog-on-a-log-1487-large.mp4",
+        previewUri = "https://mixkit.imgix.net/videos/preview/mixkit-red-frog-on-a-log-1487-0.jpg",
+    )
+
+    private val video7 = VideoData(
+        id = "7",
+        videoUri = "https://assets.mixkit.co/videos/preview/mixkit-tree-with-yellow-flowers-1173-large.mp4",
+        previewUri = "https://mixkit.imgix.net/videos/preview/mixkit-tree-with-yellow-flowers-1173-0.jpg",
+    )
+
+    private val video8 = VideoData(
+        id = "8",
+        videoUri = "https://assets.mixkit.co/videos/preview/mixkit-palm-tree-in-front-of-the-sun-1191-large.mp4",
+        previewUri = "https://mixkit.imgix.net/videos/preview/mixkit-palm-tree-in-front-of-the-sun-1191-0.jpg",
+    )
+
+    private val video9 = VideoData(
+        id = "9",
+        videoUri = "https://assets.mixkit.co/videos/preview/mixkit-red-frog-on-a-log-1487-large.mp4",
+        previewUri = "https://mixkit.imgix.net/videos/preview/mixkit-red-frog-on-a-log-1487-0.jpg",
+    )
+
+    private val video10 = VideoData(
+        id = "10",
+        videoUri = "https://assets.mixkit.co/videos/preview/mixkit-tree-with-yellow-flowers-1173-large.mp4",
+        previewUri = "https://mixkit.imgix.net/videos/preview/mixkit-tree-with-yellow-flowers-1173-0.jpg",
+    )
+
+    private val video11 = VideoData(
+        id = "11",
+        videoUri = "https://assets.mixkit.co/videos/preview/mixkit-palm-tree-in-front-of-the-sun-1191-large.mp4",
+        previewUri = "https://mixkit.imgix.net/videos/preview/mixkit-palm-tree-in-front-of-the-sun-1191-0.jpg",
+    )
+
+    private val video12 = VideoData(
+        id = "12",
+        videoUri = "https://assets.mixkit.co/videos/preview/mixkit-red-frog-on-a-log-1487-large.mp4",
+        previewUri = "https://mixkit.imgix.net/videos/preview/mixkit-red-frog-on-a-log-1487-0.jpg",
+    )
+
+    val sampleVideos = listOf(video1, video2, video3, video4, video5, video6, video7, video8, video9, video10, video11, video12)
+}

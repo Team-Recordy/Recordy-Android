1ab9054517c60481e89432230bae273be669b75c
Sangwook123 <operawook@catholic.ac.kr>
2024-07-05 20:07:51 +0900

    [feat] #47 example video screen

---
+++ feature/video/src/main/java/com/record/video/VideoContract.kt
@@ -0,0 +1,15 @@
+package com.record.video
+
+import com.record.model.VideoData
+import com.record.ui.base.SideEffect
+import com.record.ui.base.UiState
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.toImmutableList
+
+data class VideoState(
+    val videos: ImmutableList<VideoData> = SampleData.sampleVideos.toImmutableList(),
+    val isPlaying: Boolean = false,
+) : UiState
+
+sealed interface VideoSideEffect : SideEffect {
+}

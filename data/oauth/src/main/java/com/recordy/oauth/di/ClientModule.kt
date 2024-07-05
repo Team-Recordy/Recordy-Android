dd42e2d95100dedfea48fbf7a6f03c89916d086b
김명석 <twinaudtjr@naver.com>
2024-07-05 23:01:11 +0900

    [feat] #4 ClientModule

--- data/oauth/src/main/java/com/recordy/oauth/di/ClientModule.kt
+++ data/oauth/src/main/java/com/recordy/oauth/di/ClientModule.kt
@@ -7,7 +7,6 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
-
 @Module
 @InstallIn(SingletonComponent::class)
 object ClientModule {

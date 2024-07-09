3decd76e05e1609d80010d83304dcdbc04bb4f70
lsakee <lsls4868@gmail.com>
2024-07-10 00:03:17 +0900

    misc: kotlin format & reorder the parameters

--- core/ui/src/main/java/com/record/ui/scroll/OnBottomReached.kt
+++ core/ui/src/main/java/com/record/ui/scroll/OnBottomReached.kt
@@ -10,30 +10,30 @@ import kotlinx.coroutines.flow.collectLatest
 
 @Composable
 fun LazyListState.OnBottomReached(
-  // tells how many items before we reach the bottom of the list
-  // to call onLoadMore function
-  buffer: Int = 0,
-  onLoadMore: () -> Unit,
+    // tells how many items before we reach the bottom of the list
+    // to call onLoadMore function
+    buffer: Int = 0,
+    onLoadMore: () -> Unit,
 ) {
-  // Buffer must be positive.
-  // Or our list will never reach the bottom.
-  require(buffer >= 0) { "buffer cannot be negative, but was $buffer" }
+    // Buffer must be positive.
+    // Or our list will never reach the bottom.
+    require(buffer >= 0) { "buffer cannot be negative, but was $buffer" }
 
-  val shouldLoadMore = remember {
-    derivedStateOf {
-      val lastVisibleItem = layoutInfo.visibleItemsInfo.lastOrNull() ?: return@derivedStateOf false
+    val shouldLoadMore = remember {
+        derivedStateOf {
+            val lastVisibleItem = layoutInfo.visibleItemsInfo.lastOrNull() ?: return@derivedStateOf false
 
-      lastVisibleItem.index >= layoutInfo.totalItemsCount - 1 - buffer
-    }
-  }
-  LaunchedEffect(shouldLoadMore) {
-    snapshotFlow { shouldLoadMore.value }
-      .collectLatest {
-        if (it) {
-          onLoadMore()
+            lastVisibleItem.index >= layoutInfo.totalItemsCount - 1 - buffer
         }
-      }
-  }
+    }
+    LaunchedEffect(shouldLoadMore) {
+        snapshotFlow { shouldLoadMore.value }
+            .collectLatest {
+                if (it) {
+                    onLoadMore()
+                }
+            }
+    }
 }
 
 fun LazyListState.isScrolledToEnd() = layoutInfo.visibleItemsInfo.lastOrNull()?.index == layoutInfo.totalItemsCount - 1

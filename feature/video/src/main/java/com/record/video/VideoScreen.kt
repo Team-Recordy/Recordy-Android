e073d819c0601b1158c7fda35cde20b8fa833302
nagaeng <helen030816@naver.com>
2024-07-11 03:39:26 +0900

    [feat] #53 mypage tab bar 구현

--- feature/video/src/main/java/com/record/video/VideoScreen.kt
+++ feature/video/src/main/java/com/record/video/VideoScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.record.designsystem.component.videoplayer.VideoPlayer

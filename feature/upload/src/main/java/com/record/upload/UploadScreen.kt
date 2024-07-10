0eb1e3cff01d382ff8454a021953d604dff94f38
lsakee <lsls4868@gmail.com>
2024-07-10 22:17:55 +0900

    [design] #62 Base UploadScreen

--- feature/upload/src/main/java/com/record/upload/UploadScreen.kt
+++ feature/upload/src/main/java/com/record/upload/UploadScreen.kt
@@ -1,18 +1,31 @@
 package com.record.upload
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 
 @Composable
 fun UploadRoute(
     padding: PaddingValues,
     modifier: Modifier = Modifier,
 ) {
-    Box(modifier = modifier.fillMaxSize()) {
+    val context = LocalContext.current
+    VideoPickerScreen(padding = padding)
+}
+
+@Composable
+fun VideoPickerScreen(
+    padding:PaddingValues,
+    /*
+    //ToDo uiState,Onclick   
+    * */
+){
+    Box(modifier = Modifier.fillMaxSize()) {
         Text("upload")
     }
 }

c32528515261d669a2e5b06d461a060fc7b832d6
Sangwook123 <operawook@catholic.ac.kr>
2024-07-10 21:19:27 +0900

    [mod] #47 recordy button

--- core/designsystem/src/main/java/com/record/designsystem/component/button/RecordyButton.kt
+++ core/designsystem/src/main/java/com/record/designsystem/component/button/RecordyButton.kt
@@ -1,19 +1,25 @@
 package com.record.designsystem.component.button
 
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.record.designsystem.theme.RecordyTheme
 
 @Composable
 fun RecordyButton(
     text: String,
+    containerColor: Color = RecordyTheme.colors.main,
+    contentColor: Color = RecordyTheme.colors.gray09,
+    textStyle: TextStyle = RecordyTheme.typography.button1,
+    cornerShape: Dp = 12.dp,
     enabled: Boolean,
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
@@ -21,22 +27,19 @@ fun RecordyButton(
     Button(
         onClick = onClick,
         enabled = enabled,
-
         colors = ButtonDefaults.buttonColors(
-            containerColor = RecordyTheme.colors.main,
-            contentColor = RecordyTheme.colors.gray09,
+            containerColor = containerColor,
+            contentColor = contentColor,
             disabledContainerColor = RecordyTheme.colors.gray08,
             disabledContentColor = RecordyTheme.colors.gray04,
         ),
-
-        shape = RoundedCornerShape(12.dp),
+        shape = RoundedCornerShape(cornerShape),
         modifier = modifier
-            .fillMaxWidth()
-            .padding(horizontal = 16.dp),
+            .fillMaxWidth(),
     ) {
         Text(
             text = text,
-            style = RecordyTheme.typography.button1,
+            style = textStyle,
         )
     }
 }

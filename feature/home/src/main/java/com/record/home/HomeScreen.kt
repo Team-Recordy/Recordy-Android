1916904f462bf89e3affe992889825d5785c8b78
Sangwook123 <operawook@catholic.ac.kr>
2024-07-10 21:47:39 +0900

    [feat] #47 home screen example code

--- feature/home/src/main/java/com/record/home/HomeScreen.kt
+++ feature/home/src/main/java/com/record/home/HomeScreen.kt
@@ -1,18 +1,41 @@
 package com.record.home
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.record.designsystem.component.RecordyVideoThumbnail
 
 @Composable
 fun HomeRoute(
     padding: PaddingValues,
     modifier: Modifier = Modifier,
 ) {
-    Box(modifier = modifier.fillMaxSize()) {
-        Text("home")
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp)
+            .padding(bottom = padding.calculateBottomPadding()),
+    ) {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            items(SampleData.sampleVideos) { item ->
+                RecordyVideoThumbnail(
+                    imageUri = item.previewUri,
+                    isBookmarkable = true,
+                    isBookmark = false,
+                )
+            }
+        }
     }
 }

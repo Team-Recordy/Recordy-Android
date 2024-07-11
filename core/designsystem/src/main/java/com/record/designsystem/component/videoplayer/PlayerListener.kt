a2731a78b8afb4a4c26adecd715fb4956595371d
Sangwook123 <operawook@catholic.ac.kr>
2024-07-12 06:35:21 +0900

    [feat] #41 player listener

---
+++ core/designsystem/src/main/java/com/record/designsystem/component/videoplayer/PlayerListener.kt
@@ -0,0 +1,30 @@
+package com.record.designsystem.component.videoplayer
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.media3.common.PlaybackException
+import androidx.media3.common.Player
+
+@Composable
+fun PlayerListener(
+    player: Player,
+    onEvent: (Int) -> Unit
+) {
+    DisposableEffect(key1 = player) {
+        val listener = object : Player.Listener {
+            override fun onRenderedFirstFrame() {
+                super.onRenderedFirstFrame()
+                onEvent(Player.EVENT_RENDERED_FIRST_FRAME)
+            }
+
+            override fun onPlayerError(error: PlaybackException) {
+                super.onPlayerError(error)
+                onEvent(Player.EVENT_PLAYER_ERROR)
+            }
+        }
+        player.addListener(listener)
+        onDispose {
+            player.removeListener(listener)
+        }
+    }
+}

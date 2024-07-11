cbe8690fbd7fc754a9c2da273dbaf7a6457c34f1
윤서희 <appleid21@sookmyung.ac.kr>
2024-07-11 23:44:30 +0900

    [mod] #52 delete import

--- core/designsystem/src/main/java/com/record/designsystem/component/button/RecordyButtonLarge.kt
+++ core/designsystem/src/main/java/com/record/designsystem/component/button/RecordyButtonLarge.kt
@@ -8,7 +8,6 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.record.designsystem.theme.RecordyTheme
 

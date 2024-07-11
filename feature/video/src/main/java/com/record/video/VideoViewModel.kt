bbdbee924870f911f237e432f15c247498560367
Sangwook123 <operawook@catholic.ac.kr>
2024-07-12 06:40:47 +0900

    [feat] #41 video screen

--- feature/video/src/main/java/com/record/video/VideoViewModel.kt
+++ feature/video/src/main/java/com/record/video/VideoViewModel.kt
@@ -1,5 +1,47 @@
 package com.record.video
 
 import com.record.ui.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.collections.immutable.toImmutableList
+import javax.inject.Inject
 
-class VideoViewModel : BaseViewModel<VideoState, VideoSideEffect>(VideoState())
+@HiltViewModel
+class VideoViewModel @Inject constructor() : BaseViewModel<VideoState, VideoSideEffect>(VideoState()){
+    fun updateToggleState(){
+        intent{
+            copy(isAll = !isAll)
+        }
+    }
+
+    fun bookmark(id: Int){
+        intent{
+            val videos = uiState.value.videos.toList()
+            videos[videos.indexOfFirst { it.id.toInt() == id }].run {
+                copy(isBookmark = !isBookmark)
+            }
+            copy(
+                videos = videos.toImmutableList()
+            )
+        }
+    }
+
+    fun showDeleteDialog(id: Int){
+        intent{
+            copy(showDeleteDialog = true, deleteVideoId = id)
+        }
+    }
+
+    fun dismissDeleteDialog(){
+        intent{
+            copy(showDeleteDialog = false, deleteVideoId = 0)
+        }
+    }
+
+    fun showNetworkErrorSnackbar(msg: String){
+        postSideEffect(VideoSideEffect.showNetworkErrorSnackbar(msg))
+    }
+
+    fun watchVideo(id: Int){
+
+    }
+}

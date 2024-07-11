fc7d6c81b6f9d69748d1f3325bded438b097f86c
Sangwook123 <operawook@catholic.ac.kr>
2024-07-12 06:40:16 +0900

    [mod] #41 sample data

--- core/model/src/main/java/com/record/model/VideoData.kt
+++ core/model/src/main/java/com/record/model/VideoData.kt
@@ -4,4 +4,9 @@ data class VideoData(
     val id: String,
     val videoUri: String,
     val previewUri: String,
+    val location: String,
+    val userName: String,
+    val content: String,
+    val bookmarkCount: Int,
+    val isBookmark: Boolean,
 )

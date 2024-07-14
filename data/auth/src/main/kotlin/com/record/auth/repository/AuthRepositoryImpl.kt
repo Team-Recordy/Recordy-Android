2eeb8f9d16d3461a3dba6d6302c32729d21b4298
김명석 <75840431+cacaocoffee@users.noreply.github.com>
2024-07-15 00:50:45 +0900

    [feat] #70 AuthRepositoryImpl , module

---
+++ data/auth/src/main/kotlin/com/record/auth/repository/AuthRepositoryImpl.kt
@@ -0,0 +1,34 @@
+package com.record.auth.repository
+
+import com.record.auth.model.request.toData
+import com.record.auth.source.local.AuthLocalDataSource
+import com.record.auth.source.remote.AuthRemoteDataSource
+import com.recordy.auth.model.AuthAgreementEntity
+import com.recordy.auth.model.AuthToken
+import com.recordy.auth.repository.AuthRepository
+import javax.inject.Inject
+
+class AuthRepositoryImpl @Inject constructor(
+//    private val authLocalDataSource: AuthLocalDataSource,
+    private val authRemoteDataSource: AuthRemoteDataSource,
+) : AuthRepository {
+    override suspend fun signIn(platformType: String): Result<AuthToken> = runCatching {
+        authRemoteDataSource.signIn(platformType).data!!.toDomain()
+    }
+
+    override suspend fun signUp(accessToken: String, authAgreementEntity: AuthAgreementEntity): Result<Unit> = runCatching {
+        authRemoteDataSource.signUp(accessToken, requestSignUpDto = authAgreementEntity.toData())
+    }
+
+    override suspend fun checkNickname(accessToken: String, nickname: String): Result<Unit> =
+        runCatching { authRemoteDataSource.checkNickname(accessToken = accessToken, nickname = nickname) }
+
+    override suspend fun delete(accessToken: String): Result<Unit> = runCatching {
+        authRemoteDataSource.delete(accessToken)
+    }
+
+    override suspend fun logout(accessToken: String): Result<Unit> = runCatching {
+        authRemoteDataSource.logout(accessToken)
+    }
+
+}

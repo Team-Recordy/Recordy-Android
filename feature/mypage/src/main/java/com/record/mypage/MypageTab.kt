5b4fefce0f03a7d6039e4ffec5a2080c5c25932a
nagaeng <helen030816@naver.com>
2024-07-15 05:41:18 +0900

    [feat] #53 mypage view 완성
    - tab bar scaffold 남았음
    - ktlintformat 함

--- feature/mypage/src/main/java/com/record/mypage/MypageTab.kt
+++ feature/mypage/src/main/java/com/record/mypage/MypageTab.kt
@@ -3,5 +3,5 @@ package com.record.mypage
 enum class MypageTab(val displayName: String) {
     TASTE("내 취향"),
     RECORD("내 기록"),
-    BOOKMARK("북마크")
+    BOOKMARK("북마크"),
 }
--- feature/mypage/src/main/java/com/record/mypage/screen/RecordScreen.kt
+++ feature/mypage/src/main/java/com/record/mypage/screen/RecordScreen.kt
@@ -1,40 +1,28 @@
 package com.record.mypage.screen
 
-import android.widget.Space
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.record.designsystem.component.RecordyVideoThumbnail
 import com.record.designsystem.theme.RecordyTheme
 import com.record.model.VideoData
-import com.record.mypage.MypageState
-import com.record.mypage.R
-import com.record.mypage.VideoItem
 
 @Composable
 fun RecordScreen(videoItems: List<VideoData>, recordCount: Int) {
@@ -60,7 +48,7 @@ fun RecordScreen(videoItems: List<VideoData>, recordCount: Int) {
                 contentAlignment = Alignment.TopEnd,
             ) {
                 Text(
-                    text =  buildRecordCountText(recordCount),
+                    text = buildRecordCountText(recordCount),
                     style = RecordyTheme.typography.body2M,
                     color = RecordyTheme.colors.gray01,
                 )
@@ -113,7 +101,8 @@ fun PreviewRecordEmptyScreen() {
 fun PreviewRecordScreen() {
     RecordyTheme {
         RecordScreen(
-            SampleData.sampleVideos, SampleData.sampleVideos.size,
+            SampleData.sampleVideos,
+            SampleData.sampleVideos.size,
         )
     }
 }

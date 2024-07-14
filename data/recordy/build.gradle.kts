e5a53fcdd9ee314887218d9e6ec10ff770fa49ae
김명석 <75840431+cacaocoffee@users.noreply.github.com>
2024-07-15 03:30:52 +0900

    [fix] #70  gradle, BuildConfigFieldsProviderImpl fix

--- data/recordy/build.gradle.kts
+++ data/recordy/build.gradle.kts
@@ -11,11 +11,10 @@ dependencies {
     implementation(projects.core.network)
     implementation(projects.core.model)
     implementation(projects.core.datastore)
-    implementation(projects.domain.auth)
+    implementation(projects.domain.recordy)
 
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.retrofit.core)
     implementation(libs.retrofit.kotlin.serialization)
     implementation(libs.okhttp.logging)
-    implementation(libs.kakao.login)
 }

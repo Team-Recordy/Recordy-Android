f271645f47c52aae1be88389f55bb5d851b06328
Sangwook123 <operawook@catholic.ac.kr>
2024-07-16 09:11:22 +0900

    [feat] #82 core model - cursor, videoUrl

---
+++ core/model/src/main/java/com/record/model/Cursor.kt
@@ -0,0 +1,7 @@
+package com.record.model
+
+data class Cursor<T>(
+    val hasNext: Boolean,
+    val nextCursor: Int,
+    val data: List<T>
+)
---
+++ core/model/src/main/java/com/record/model/VideoUrl.kt
@@ -0,0 +1,6 @@
+package com.record.model
+
+data class VideoUrl(
+    val videoUri: String,
+    val previewUri: String,
+)

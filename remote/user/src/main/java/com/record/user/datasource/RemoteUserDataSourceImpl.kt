5438ccfcb90a97d4f80248391e8289ef3a1a0c80
Sangwook123 <operawook@catholic.ac.kr>
2024-07-16 09:16:29 +0900

    [feat] #82 user datasource

---
+++ remote/user/src/main/java/com/record/user/datasource/RemoteUserDataSourceImpl.kt
@@ -0,0 +1,21 @@
+package com.record.user.datasource
+
+import com.record.user.api.UserApi
+import com.record.user.model.remote.response.ResponseGetFollowerListDto
+import com.record.user.model.remote.response.ResponseGetFollowingListDto
+import com.record.user.model.remote.response.ResponseGetUserProfileDto
+import com.record.user.model.remote.response.ResponsePostFollowDto
+import com.record.user.source.remote.RemoteUserDataSource
+import javax.inject.Inject
+
+class RemoteUserDataSourceImpl @Inject constructor(
+    private val userApi: UserApi,
+) : RemoteUserDataSource {
+    override fun getFollowingList(cursorId: Long, size: Int): ResponseGetFollowingListDto = userApi.getFollowingList(cursorId, size)
+
+    override fun getFollowerList(cursorId: Long, size: Int): ResponseGetFollowerListDto = userApi.getFollowerList(cursorId, size)
+
+    override fun postFollow(followingId: Long): ResponsePostFollowDto = userApi.postFollow(followingId)
+
+    override fun getUserProfile(userId: Long): ResponseGetUserProfileDto = userApi.getUserProfileDto(userId)
+}

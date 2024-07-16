33075054e88a5f965cd6aaa3f931bf36ae4e9d88
Sangwook123 <operawook@catholic.ac.kr>
2024-07-16 09:13:26 +0900

    [feat] #82 user domain module

---
+++ domain/user/src/main/java/com/record/user/model/Profile.kt
@@ -0,0 +1,11 @@
+package com.record.user.model
+
+data class Profile(
+    val id: Int,
+    val followerCount: Int,
+    val followingCount: Int,
+    val isFollowing: Boolean,
+    val nickname: String,
+    val profileImageUrl: String?,
+    val recordCount: Int,
+)

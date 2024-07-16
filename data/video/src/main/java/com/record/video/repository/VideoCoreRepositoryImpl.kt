98ce713c7f116ca3563668fcc0cab9d015d39f3c
Sangwook123 <operawook@catholic.ac.kr>
2024-07-16 09:20:03 +0900

    [feat] #82 video repository impl

---
+++ data/video/src/main/java/com/record/video/repository/VideoCoreRepositoryImpl.kt
@@ -0,0 +1,38 @@
+package com.record.video.repository
+
+import com.record.model.exception.ApiError
+import com.record.video.source.remote.RemoteVideoCoreDataSource
+import retrofit2.HttpException
+import javax.inject.Inject
+
+class VideoCoreRepositoryImpl @Inject constructor(
+    private val remoteVideoCoreDataSource: RemoteVideoCoreDataSource,
+) : VideoCoreRepository {
+    override fun deleteVideo(id: Long): Result<Unit> = runCatching {
+        remoteVideoCoreDataSource.deleteVideo(id)
+    }.recoverCatching { exception ->
+        when (exception) {
+            is HttpException -> {
+                throw ApiError(exception.message())
+            }
+
+            else -> {
+                throw exception
+            }
+        }
+    }
+
+    override fun watchVideo(id: Long): Result<Unit> = runCatching{
+        remoteVideoCoreDataSource.watchVideo(id)
+    }.recoverCatching { exception ->
+        when (exception) {
+            is HttpException -> {
+                throw ApiError(exception.message())
+            }
+
+            else -> {
+                throw exception
+            }
+        }
+    }
+}

50c83f22efc28b771030c8b651a81555dd037af1
Sangwook123 <operawook@catholic.ac.kr>
2024-07-16 23:07:05 +0900

    [feat] #92 data user

---
+++ data/user/src/main/java/com/record/user/model/remote/response/ResponseGetUserPreferenceDto.kt
@@ -0,0 +1,13 @@
+package com.record.user.model.remote.response
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ResponseGetUserPreferenceDto(
+    @SerialName("preference")
+    val preference: List<List<String>>,
+    @SerialName("userId")
+    val userId: Int
+)
--- data/user/src/main/java/com/record/user/repository/UserRepositoryImpl.kt
+++ data/user/src/main/java/com/record/user/repository/UserRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.record.user.repository
 
 import com.record.model.Cursor
 import com.record.model.exception.ApiError
+import com.record.user.model.Preference
 import com.record.user.model.Profile
 import com.record.user.model.User
 import com.record.user.model.remote.response.toCore
@@ -48,7 +49,7 @@ class UserRepositoryImpl @Inject constructor(
     override fun postFollow(followingId: Long): Result<Boolean> = runCatching {
         remoteUserDataSource.postFollow(followingId)
     }.map {
-        it.isFollowing
+        it
     }.recoverCatching { exception ->
         when (exception) {
             is HttpException -> {
@@ -65,6 +66,26 @@ class UserRepositoryImpl @Inject constructor(
         remoteUserDataSource.getUserProfile(userId)
     }.mapCatching {
         it.toDomain()
+    }.recoverCatching { exception ->
+        when (exception) {
+            is HttpException -> {
+                throw ApiError(exception.message())
+            }
+
+            else -> {
+                throw exception
+            }
+        }
+    }
+
+    override fun getUserPreference(): Result<Triple<Preference, Preference, Preference>> = runCatching {
+        remoteUserDataSource.getUserPreference().preference
+    }.mapCatching {
+        Triple(
+            Preference(it[0][0], it[0][1].toInt()),
+            Preference(it[1][0], it[1][1].toInt()),
+            Preference(it[2][0], it[2][1].toInt()),
+        )
     }.recoverCatching { exception ->
         when (exception) {
             is HttpException -> {

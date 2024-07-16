13843f23b478a374cececaeebe23a8197ebae4ea
Sangwook123 <operawook@catholic.ac.kr>
2024-07-16 23:05:17 +0900

    [feat] #92 user remote

--- remote/user/src/main/java/com/record/user/datasource/RemoteUserDataSourceImpl.kt
+++ remote/user/src/main/java/com/record/user/datasource/RemoteUserDataSourceImpl.kt
@@ -3,6 +3,7 @@ package com.record.user.datasource
 import com.record.user.api.UserApi
 import com.record.user.model.remote.response.ResponseGetFollowerListDto
 import com.record.user.model.remote.response.ResponseGetFollowingListDto
+import com.record.user.model.remote.response.ResponseGetUserPreferenceDto
 import com.record.user.model.remote.response.ResponseGetUserProfileDto
 import com.record.user.model.remote.response.ResponsePostFollowDto
 import com.record.user.source.remote.RemoteUserDataSource
@@ -15,7 +16,9 @@ class RemoteUserDataSourceImpl @Inject constructor(
 
     override fun getFollowerList(cursorId: Long, size: Int): ResponseGetFollowerListDto = userApi.getFollowerList(cursorId, size)
 
-    override fun postFollow(followingId: Long): ResponsePostFollowDto = userApi.postFollow(followingId)
+    override fun postFollow(followingId: Long): Boolean = userApi.postFollow(followingId)
 
     override fun getUserProfile(userId: Long): ResponseGetUserProfileDto = userApi.getUserProfileDto(userId)
+
+    override fun getUserPreference(): ResponseGetUserPreferenceDto = userApi.getUserPreference()
 }

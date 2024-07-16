ad2d8c4ef1b52827638aa1069bed1f00e306d41b
Sangwook123 <operawook@catholic.ac.kr>
2024-07-16 23:04:29 +0900

    [feat] #92 video domain

--- domain/video/src/main/java/com/record/video/repository/VideoRepository.kt
+++ domain/video/src/main/java/com/record/video/repository/VideoRepository.kt
@@ -1,9 +1,15 @@
 package com.record.video.repository
 
 import com.record.model.Cursor
+import com.record.model.Page
 import com.record.video.model.VideoData
 
 interface VideoRepository {
     fun getAllVideos(cursorId: Long, pageSize: Int): Result<List<VideoData>>
     fun getRecentVideos(keywords: List<String>, pageNumber: Int, pageSize: Int): Result<Cursor<VideoData>>
+    fun getPopularVideos(keywords: List<String>, pageNumber: Int, pageSize: Int): Result<Page<VideoData>>
+    fun getUserVideos(otherUserId: Long, cursorId: Long, size: Int): Result<Cursor<VideoData>>
+    fun getFollowingVideos(userId: Long, cursorId: Long, size: Int): Result<Cursor<VideoData>>
+    fun getBookmarkVideos(cursorId: Long, size: Int): Result<Cursor<VideoData>>
+    fun bookmark(videoId: Long): Result<Boolean>
 }

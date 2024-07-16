6b0f132056a54d954c1a5a6402bd10ce67758695
Sangwook123 <operawook@catholic.ac.kr>
2024-07-16 09:18:35 +0900

    [feat] #82 data - video dto

---
+++ data/video/src/main/java/com/record/video/model/remote/request/RequestPostVideoDto.kt
@@ -0,0 +1,30 @@
+package com.record.video.model.remote.request
+
+
+import com.record.network.model.FileUrl
+import com.record.upload.model.VideoInfo
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import java.io.File
+
+@Serializable
+data class RequestPostVideoDto(
+    @SerialName("content")
+    val content: String,
+    @SerialName("fileUrl")
+    val fileUrl: FileUrl,
+    @SerialName("keywords")
+    val keywords: List<String>,
+    @SerialName("location")
+    val location: String
+)
+
+fun VideoInfo.toData() = RequestPostVideoDto(
+    content = content,
+    fileUrl = FileUrl(
+        videoUrl = videoUrl,
+        thumbnailUrl = previewUrl,
+    ),
+    keywords = keywords,
+    location = location,
+)
---
+++ data/video/src/main/java/com/record/video/model/remote/response/ResponseGetPopularVideoDto.kt
@@ -0,0 +1,15 @@
+package com.record.video.model.remote.response
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ResponseGetPopularVideoDto(
+    @SerialName("content")
+    val content: List<ResponseGetVideoDto>,
+    @SerialName("hasNext")
+    val hasNext: Boolean,
+    @SerialName("pageNumber")
+    val pageNumber: Int,
+)

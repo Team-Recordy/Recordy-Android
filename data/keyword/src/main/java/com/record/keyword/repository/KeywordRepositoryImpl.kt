e22bb2c33f0f9f9564a8869c4b110258aefbb5fd
Sangwook123 <operawook@catholic.ac.kr>
2024-07-16 23:06:48 +0900

    [feat] #92 data keyword

---
+++ data/keyword/src/main/java/com/record/keyword/repository/KeywordRepositoryImpl.kt
@@ -0,0 +1,27 @@
+package com.record.keyword.repository
+
+import com.record.keyword.model.PreferenceKeyword
+import com.record.keyword.source.RemoteKeywordDataSource
+import com.record.model.exception.ApiError
+import retrofit2.HttpException
+import javax.inject.Inject
+
+class KeywordRepositoryImpl @Inject constructor(
+    private val remoteKeywordDataSource: RemoteKeywordDataSource,
+) : KeywordRepository {
+    override fun getKeywords(): Result<PreferenceKeyword> = runCatching {
+        remoteKeywordDataSource.getKeywords()
+    }.mapCatching {
+        PreferenceKeyword(keywords = it)
+    }.recoverCatching { exception ->
+        when (exception) {
+            is HttpException -> {
+                throw ApiError(exception.message())
+            }
+
+            else -> {
+                throw exception
+            }
+        }
+    }
+}

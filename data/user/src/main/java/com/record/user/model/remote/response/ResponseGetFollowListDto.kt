2447789bd79f8808e07cc536be450d58fd931a35
Sangwook123 <operawook@catholic.ac.kr>
2024-07-16 09:15:30 +0900

    [feat] #82 data - user dto

---
+++ data/user/src/main/java/com/record/user/model/remote/response/ResponseGetFollowListDto.kt
@@ -0,0 +1,38 @@
+package com.record.user.model.remote.response
+
+
+import com.record.model.Cursor
+import com.record.user.model.User
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ResponseGetFollowerListDto(
+    @SerialName("content")
+    val content: List<Content>,
+    @SerialName("hasNext")
+    val hasNext: Boolean,
+    @SerialName("nextCursor")
+    val nextCursor: Int
+) {
+    @Serializable
+    data class Content(
+        @SerialName("following")
+        val following: Boolean,
+        @SerialName("userInfo")
+        val userInfo: UserInfo
+    )
+}
+
+fun ResponseGetFollowerListDto.toCore() = Cursor(
+    hasNext = hasNext,
+    nextCursor = nextCursor,
+    data = content.map {
+        User(
+            id = it.userInfo.id,
+            nickname = it.userInfo.nickname,
+            isFollowing = it.following,
+            profileImageUri = it.userInfo.profileImageUrl,
+        )
+    }
+)

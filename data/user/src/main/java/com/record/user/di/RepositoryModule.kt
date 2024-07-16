28e4d3116545a1f8d1dd4e6c35a4287d14f16644
Sangwook123 <operawook@catholic.ac.kr>
2024-07-16 09:17:44 +0900

    [feat] #82 user repository

---
+++ data/user/src/main/java/com/record/user/di/RepositoryModule.kt
@@ -0,0 +1,17 @@
+package com.record.user.di
+
+import com.record.user.repository.UserRepository
+import com.record.user.repository.UserRepositoryImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+    @Binds
+    @Singleton
+    abstract fun bindsUserRepository(userRepositoryImpl: UserRepositoryImpl): UserRepository
+}

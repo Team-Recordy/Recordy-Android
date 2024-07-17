1079acff040c108b5024e73c3992dab40e216f02
Sangwook123 <operawook@catholic.ac.kr>
2024-07-17 13:23:34 +0900

    [chore] #98 video data

--- domain/video/src/main/java/com/record/video/model/VideoData.kt
+++ domain/video/src/main/java/com/record/video/model/VideoData.kt
@@ -1,14 +1,14 @@
 package com.record.video.model
 
 data class VideoData(
-    val id: Int,
+    val id: Long,
     val isBookmark: Boolean,
     val bookmarkCount: Int,
     val content: String,
     val videoUrl: String,
     val previewUrl: String,
     val location: String,
-    val uploaderId: Int,
+    val uploaderId: Long,
     val nickname: String,
     val isMine: Boolean,
 )

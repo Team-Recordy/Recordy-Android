f94818491e23de18db07a0b9a2e4a06ac015a7e4
Sangwook123 <operawook@catholic.ac.kr>
2024-07-17 13:20:33 +0900

    [feat] #98 pager bottom reached util

--- core/ui/src/main/java/com/record/ui/scroll/OnBottomReached.kt
+++ core/ui/src/main/java/com/record/ui/scroll/OnBottomReached.kt
@@ -1,11 +1,15 @@
 package com.record.ui.scroll
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.pager.PagerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.platform.LocalDensity
 import kotlinx.coroutines.flow.collectLatest
 
 @Composable
@@ -37,3 +41,33 @@ fun LazyListState.OnBottomReached(
 }
 
 fun LazyListState.isScrolledToEnd() = layoutInfo.visibleItemsInfo.lastOrNull()?.index == layoutInfo.totalItemsCount - 1
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun PagerState.onBottomReached(
+    buffer: Int = 0, // Buffer in density-independent pixels (dp)
+    onLoadMore: () -> Unit,
+) {
+    val density = LocalDensity.current
+
+    require(buffer >= 0) { "buffer cannot be negative, but was $buffer" }
+
+    // Collect the derived state to trigger when we reach the end of the list
+    val shouldLoadMore by remember {
+        derivedStateOf {
+            val lastVisibleItem = layoutInfo.visiblePagesInfo.lastOrNull() ?: return@derivedStateOf false
+            val totalItemsCount = pageCount // Replace with your actual total item count
+
+            lastVisibleItem.index >= totalItemsCount - 1 - buffer
+        }
+    }
+
+    LaunchedEffect(shouldLoadMore) {
+        snapshotFlow { shouldLoadMore }
+            .collectLatest {
+                if (it) {
+                    onLoadMore()
+                }
+            }
+    }
+}

912f3c7af36d73cb111ebe523544383cf9e3ac6b
Sangwook123 <operawook@catholic.ac.kr>
2024-07-17 13:17:52 +0900

    [add] #98 encoding util

--- core/common/src/main/java/com/record/common/util/EncodingUtil.kt
+++ core/common/src/main/java/com/record/common/util/EncodingUtil.kt
@@ -1,6 +1,16 @@
 package com.record.common.util
 
+import java.util.Base64
+
 fun toUTF8HexString(input: String): String {
     val utf8Bytes = input.toByteArray(Charsets.UTF_8)
     return utf8Bytes.joinToString(separator = "") { byte -> "%02x".format(byte) }
+}
+
+fun toUTF8(input: String): String {
+    val bytes = input.toByteArray(Charsets.UTF_8)
+
+    val base64EncodedString = Base64.getEncoder().encodeToString(bytes)
+
+    return base64EncodedString
 }

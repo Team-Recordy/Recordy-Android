d60e2545c418cb266003ff7c316b2f3e9178fe4d
Sangwook123 <operawook@catholic.ac.kr>
2024-07-17 13:26:01 +0900

    [feat] #98 video detail server connect

--- feature/video/src/main/java/com/record/video/videodetail/VideoDetailContract.kt
+++ feature/video/src/main/java/com/record/video/videodetail/VideoDetailContract.kt
@@ -1,24 +1,32 @@
 package com.record.video.videodetail
 
-import com.record.model.VideoData
 import com.record.model.VideoType
 import com.record.ui.base.SideEffect
 import com.record.ui.base.UiState
-import com.record.video.SampleData
+import com.record.video.model.VideoData
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.toImmutableList
 
 data class VideoDetailState(
-    val videos: ImmutableList<VideoData> = SampleData.sampleVideos.toImmutableList(),
+    val videos: ImmutableList<VideoData> = emptyList<VideoData>().toImmutableList(),
     val videoType: VideoType = VideoType.MY,
     val isPlaying: Boolean = false,
     val showDeleteDialog: Boolean = false,
-    val deleteVideoId: Int = 0,
-    val observingId: Int = 0,
+    val deleteVideoId: Long = 0,
+    val observingIndex: Int = 0,
+    val observingId: Long = 0,
+    val page: Int = 0,
+    val keyword: String = "",
+    val cursor: Long = 0,
+    val userId: Long = 0,
+    val isEnd: Boolean = false,
+    val init: Boolean = false,
 ) : UiState
 
 sealed interface VideoDetailSideEffect : SideEffect {
     data class ShowNetworkErrorSnackbar(val msg: String) : VideoDetailSideEffect
     data object NavigateToMypage : VideoDetailSideEffect
     data class NavigateToUserProfile(val id: Int) : VideoDetailSideEffect
+    data class InitialPagerState(val index: Int) : VideoDetailSideEffect
+    data class MovePage(val index: Int) : VideoDetailSideEffect
 }

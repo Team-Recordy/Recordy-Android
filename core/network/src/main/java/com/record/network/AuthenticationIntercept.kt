9ab8514fe85d5beea4e0db881d4f0e80814dca47
lsakee <lsls4868@gmail.com>
2024-07-19 08:07:47 +0900

    [feat] #62 fix buildConfigFieldProvider

--- core/network/src/main/java/com/record/network/AuthenticationIntercept.kt
+++ core/network/src/main/java/com/record/network/AuthenticationIntercept.kt
@@ -1,6 +1,11 @@
 package com.record.network
 
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import com.record.common.buildconfig.BuildConfigFieldProvider
 import com.record.datastore.token.TokenDataStore
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
@@ -10,12 +15,17 @@ import javax.inject.Inject
 
 class AuthenticationIntercept @Inject constructor(
     private val datastore: TokenDataStore,
+    @ApplicationContext private val context: Context,
+    private val buildConfigFieldProvider: BuildConfigFieldProvider,
 ) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val originalRequest = chain.request()
         val authRequest = handleRequest(originalRequest)
         val response = chain.proceed(authRequest)
-
+        if (response.code == 200 && originalRequest.url.toString() == "${buildConfigFieldProvider.get().baseUrl}/api/v1/records") {
+            Log.d("test", "test1")
+            sendUploadResultBroadcast("success")
+        } else if (originalRequest.url.toString() == "${buildConfigFieldProvider.get().baseUrl}/api/v1/records") sendUploadResultBroadcast("업로드 실패했습니다.")
         return response
     }
 
@@ -23,4 +33,11 @@ class AuthenticationIntercept @Inject constructor(
 
     private fun Request.accessTokenBuilder() =
         this.newBuilder().addHeader("Authorization", runBlocking { "Bearer ${datastore.token.first().accessToken}" }).build()
+
+    private fun sendUploadResultBroadcast(message: String) {
+        val intent = Intent("com.example.UPLOAD_RESULT").apply {
+            putExtra("message", message)
+        }
+        context.sendBroadcast(intent)
+    }
 }

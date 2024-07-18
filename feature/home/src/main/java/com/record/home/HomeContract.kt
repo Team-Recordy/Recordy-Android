a12db21a3aea9c9cd3a6fb836ba7eeeaa4f12fa1
Sangwook123 <operawook@catholic.ac.kr>
2024-07-18 13:20:16 +0900

    [feat] #102 home api connect

--- feature/home/src/main/java/com/record/home/HomeContract.kt
+++ feature/home/src/main/java/com/record/home/HomeContract.kt
@@ -4,17 +4,18 @@ import com.record.model.VideoType
 import com.record.ui.base.SideEffect
 import com.record.ui.base.UiState
 import com.record.video.model.VideoData
+import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.toImmutableList
 
 data class HomeState(
-    val chipList: List<String> = emptyList<String>().toImmutableList(),
-    val popularList: List<VideoData> = emptyList<VideoData>().toImmutableList(),
-    val recentList: List<VideoData> = emptyList<VideoData>().toImmutableList(),
+    val chipList: ImmutableList<String> = emptyList<String>().toImmutableList(),
+    val popularList: ImmutableList<VideoData> = emptyList<VideoData>().toImmutableList(),
+    val recentList: ImmutableList<VideoData> = emptyList<VideoData>().toImmutableList(),
     val selectedChipIndex: Int? = null,
     val isLoading: Boolean = false,
 ) : UiState
 
 sealed interface HomeSideEffect : SideEffect {
     data object navigateToUpload : HomeSideEffect
-    data class navigateToVideo(val index: Long, val type: VideoType, val keyword: String?) : HomeSideEffect
+    data class navigateToVideo(val id: Long, val type: VideoType, val keyword: String?) : HomeSideEffect
 }

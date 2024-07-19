06a9836f130870ab56c5f9e9ca1dfeeb0b290e5d
lsakee <lsls4868@gmail.com>
2024-07-19 11:31:28 +0900

    [feat] #62 add domain,data

--- remote/video/src/main/java/com/record/video/api/BucketApi.kt
+++ remote/video/src/main/java/com/record/video/api/BucketApi.kt
@@ -15,4 +15,10 @@ interface BucketApi {
         @Body requestBody: RequestBody,
         @Header("Content-Type") contentType: String = "application/octet-stream",
     ): Response<ResponseBody>
+    @PUT
+    suspend fun uploadThumbnailWithS3Video(
+        @Url url: String,
+        @Body requestBody: RequestBody,
+        @Header("Content-Type") contentType: String = "application/octet-stream",
+    ): Response<ResponseBody>
 }

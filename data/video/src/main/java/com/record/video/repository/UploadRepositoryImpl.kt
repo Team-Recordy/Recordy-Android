5f1deb0f9809caaf41d7feee668625644be1d0eb
lsakee <lsls4868@gmail.com>
2024-07-19 10:46:29 +0900

    [feat] #62 separate s3 thumbnail upload with architecture

--- data/video/src/main/java/com/record/video/repository/UploadRepositoryImpl.kt
+++ data/video/src/main/java/com/record/video/repository/UploadRepositoryImpl.kt
@@ -57,4 +57,18 @@ class UploadRepositoryImpl @Inject constructor(
                 }
             }
         }
+
+    override suspend fun uploadThumbnailToS3Bucket(url: String, file: File): Result<String> =
+    runCatching {
+        remoteUploadDataSource.uploadThumbnailToS3Bucket(url, file)
+    }.recoverCatching { exception ->
+        when (exception) {
+            is HttpException -> {
+                throw ApiError(exception.message())
+            }
+            else -> {
+                throw exception
+            }
+        }
+    }
 }
--- remote/video/src/main/java/com/record/video/datasource/RemoteUploadDataSourceImpl.kt
+++ remote/video/src/main/java/com/record/video/datasource/RemoteUploadDataSourceImpl.kt
@@ -2,16 +2,15 @@ package com.record.video.datasource
 
 import android.content.Context
 import android.util.Log
+import com.record.common.util.getVideoFrameAt1Sec
 import com.record.video.api.BucketApi
 import com.record.video.api.UploadApi
 import com.record.video.model.remote.request.RequestPostVideoDto
 import com.record.video.model.remote.response.ResponseGetPresignedUrlDto
 import com.record.video.source.remote.RemoteUploadDataSource
+import dagger.hilt.android.qualifiers.ApplicationContext
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
-import okhttp3.MultipartBody
 import okhttp3.RequestBody
-import okhttp3.RequestBody.Companion.toRequestBody
-import okhttp3.ResponseBody.Companion.toResponseBody
 import java.io.File
 import java.net.URL
 import javax.inject.Inject
@@ -19,6 +18,7 @@ import javax.inject.Inject
 class RemoteUploadDataSourceImpl @Inject constructor(
     private val uploadApi: UploadApi,
     private val bucketApi: BucketApi,
+    @ApplicationContext private val context: Context
 ) : RemoteUploadDataSource {
     override suspend fun getUploadUrl(): ResponseGetPresignedUrlDto =
         uploadApi.getPresignedUploadUrl()
@@ -34,5 +34,17 @@ class RemoteUploadDataSourceImpl @Inject constructor(
         val url = URL(bucketApi.uploadVideoWithS3Video(url, requestBody).raw().request.url.toString())
         Log.d("testUrl","$url")
        return URL(url.protocol, url.host, url.port, url.path).toString()
+    }
+
+    override suspend fun uploadThumbnailToS3Bucket(url: String, file: File): String {
+        val videoPath = file.absolutePath
+        val outputImagePath = File(context.cacheDir, file.name)
+        getVideoFrameAt1Sec(videoPath, outputImagePath.absolutePath)
+        val mediaType = "application/octet-stream".toMediaTypeOrNull()
+        val requestBody = RequestBody.create(mediaType, outputImagePath)
+        val url = URL(bucketApi.uploadVideoWithS3Video(url, requestBody).raw().request.url.toString())
+        Log.d("testUrl2","$url")
+        return URL(url.protocol, url.host, url.port, url.path).toString()
+
     }
 }

23614b10ca23f5942de1f6beb6eb4a29e15b439d
Sangwook123 <operawook@catholic.ac.kr>
2024-07-24 02:30:03 +0900

    [add] #118 workmanager module

--- app/src/main/java/com/record/recordy/RecordyApplication.kt
+++ app/src/main/java/com/record/recordy/RecordyApplication.kt
@@ -2,13 +2,19 @@ package com.record.recordy
 
 import android.app.Application
 import android.util.Log
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import com.kakao.sdk.common.KakaoSdk
 import com.kakao.sdk.common.util.Utility
 import com.record.buildconfig.BuildConfig.KAKAO_NATIVE_KEY
+import dagger.hilt.EntryPoint
+import dagger.hilt.EntryPoints
+import dagger.hilt.InstallIn
 import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.components.SingletonComponent
 
 @HiltAndroidApp
-class RecordyApplication : Application() {
+class RecordyApplication : Application(), Configuration.Provider {
     override fun onCreate() {
         super.onCreate()
         setKakaoSdk()
@@ -19,4 +25,14 @@ class RecordyApplication : Application() {
     private fun setKakaoSdk() {
         KakaoSdk.init(this, KAKAO_NATIVE_KEY)
     }
+
+    @EntryPoint
+    @InstallIn(SingletonComponent::class)
+    interface HiltWorkerFactoryEntryPoint {
+        fun workerFactory(): HiltWorkerFactory
+    }
+
+    override val workManagerConfiguration: Configuration = Configuration.Builder()
+        .setWorkerFactory(EntryPoints.get(this, HiltWorkerFactoryEntryPoint::class.java).workerFactory())
+        .build()
 }

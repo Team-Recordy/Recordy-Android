ad2f3af171f2c5de87e22875cf3eb8bc1abfde8d
Sangwook123 <operawook@catholic.ac.kr>
2024-07-24 08:25:37 +0900

    [del] #118 unused import

--- feature/navigator/src/main/java/com/record/navigator/MainScreen.kt
+++ feature/navigator/src/main/java/com/record/navigator/MainScreen.kt
@@ -33,7 +33,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle

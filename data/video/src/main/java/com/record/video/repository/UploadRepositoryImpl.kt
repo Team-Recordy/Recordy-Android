c55b7a5c367b8048fd1a58ebec6c8679e4f5ae74
Sangwook123 <operawook@catholic.ac.kr>
2024-07-24 08:10:30 +0900

    [feat] #118 get gallery videos repository

--- data/video/src/main/java/com/record/video/repository/UploadRepositoryImpl.kt
+++ data/video/src/main/java/com/record/video/repository/UploadRepositoryImpl.kt
@@ -1,14 +1,36 @@
 package com.record.video.repository
 
+import com.record.model.exception.ApiError
+import com.record.upload.model.GallercyVideo
 import com.record.upload.model.RecordInfo
 import com.record.upload.repository.UploadRepository
+import com.record.video.model.local.toDomain
+import com.record.video.source.local.LocalVideoDataSource
 import com.record.workmanager.upload.UploadEnqueuer
+import java.io.IOException
 import javax.inject.Inject
 
 class UploadRepositoryImpl @Inject constructor(
     private val uploadEnqueuer: UploadEnqueuer,
+    private val localVideoDataSource: LocalVideoDataSource,
 ) : UploadRepository {
     override suspend fun upload(recordInfo: RecordInfo) {
         uploadEnqueuer.enqueueUploadWork(recordInfo.videoPath, recordInfo.location, recordInfo.keywords, recordInfo.content)
+    }
+
+    override suspend fun getVideosFromGallery(page: Int, loadSize: Int, currentLocation: String?): Result<List<GallercyVideo>> = runCatching {
+        localVideoDataSource.getVideosFromGallery(page, loadSize, currentLocation)
+    }.mapCatching {
+        it.map { it.toDomain() }
+    }.recoverCatching { exception ->
+        when (exception) {
+            is IOException -> {
+                throw ApiError("IOException")
+            }
+
+            else -> {
+                throw exception
+            }
+        }
     }
 }

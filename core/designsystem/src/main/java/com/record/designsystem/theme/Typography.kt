0064ad5c9a33f9937686d3b9c737a35958b17389
나갱 <109855280+nagaeng@users.noreply.github.com>
2024-09-13 15:44:18 +0900

    [add] #123 typo 수정

--- core/designsystem/src/main/java/com/record/designsystem/theme/Typography.kt
+++ core/designsystem/src/main/java/com/record/designsystem/theme/Typography.kt
@@ -14,10 +14,10 @@ import androidx.compose.ui.unit.sp
 import com.record.designsystem.R
 
 val PretendardBold = FontFamily(Font(R.font.pretendard_bold, FontWeight.Bold))
+val PretendardExtraBold = FontFamily(Font(R.font.pretendard_extrabold, FontWeight.ExtraBold))
 val PretendardSemiBold = FontFamily(Font(R.font.pretendard_semibold, FontWeight.SemiBold))
 val PretendardMedium = FontFamily(Font(R.font.pretendard_medium, FontWeight.Medium))
 val PretendardRegular = FontFamily(Font(R.font.pretendard_regular, FontWeight.Normal))
-val CocogooseproThin = FontFamily(Font(R.font.cocogoosepro_thin, FontWeight.Thin))
 
 @Stable
 class RecordyTypography internal constructor(
@@ -25,25 +25,30 @@ class RecordyTypography internal constructor(
     title1: TextStyle,
     title2: TextStyle,
     title3: TextStyle,
+    title3SB: TextStyle,
+    title4: TextStyle,
     subtitle: TextStyle,
+    emptybodyB: TextStyle,
     body1M: TextStyle,
     body1R: TextStyle,
     body2M: TextStyle,
+    body2SB: TextStyle,
     body2B: TextStyle,
     body2L: TextStyle,
-    caption1: TextStyle,
+    caption1R: TextStyle,
+    caption1M: TextStyle,
     caption1U: TextStyle,
-    caption2: TextStyle,
+    caption1SB: TextStyle,
+    caption2R: TextStyle,
+    caption2M: TextStyle,
     button1: TextStyle,
     button2: TextStyle,
     button2B: TextStyle,
-    number1: TextStyle,
-    number2: TextStyle,
-    number3: TextStyle,
     keyword1: TextStyle,
     keyword2: TextStyle,
     keyword3: TextStyle,
     emptybody: TextStyle,
+
 ) {
     var headline: TextStyle by mutableStateOf(headline)
         private set
@@ -53,7 +58,13 @@ class RecordyTypography internal constructor(
         private set
     var title3: TextStyle by mutableStateOf(title3)
         private set
+    var title3SB: TextStyle by mutableStateOf(title3SB)
+        private set
+    var title4: TextStyle by mutableStateOf(title4)
+        private set
     var subtitle: TextStyle by mutableStateOf(subtitle)
+        private set
+    var emptybodyB: TextStyle by mutableStateOf(emptybodyB)
         private set
     var body1M: TextStyle by mutableStateOf(body1M)
         private set
@@ -61,27 +72,29 @@ class RecordyTypography internal constructor(
         private set
     var body2M: TextStyle by mutableStateOf(body2M)
         private set
+    var body2SB: TextStyle by mutableStateOf(body2SB)
+        private set
     var body2B: TextStyle by mutableStateOf(body2B)
         private set
     var body2L: TextStyle by mutableStateOf(body2L)
         private set
-    var caption1: TextStyle by mutableStateOf(caption1)
+    var caption1R: TextStyle by mutableStateOf(caption1R)
+        private set
+    var caption1M: TextStyle by mutableStateOf(caption1M)
         private set
     var caption1U: TextStyle by mutableStateOf(caption1U)
         private set
-    var caption2: TextStyle by mutableStateOf(caption2)
+    var caption1SB: TextStyle by mutableStateOf(caption1SB)
+        private set
+    var caption2R: TextStyle by mutableStateOf(caption2R)
+        private set
+    var caption2M: TextStyle by mutableStateOf(caption2M)
         private set
     var button1: TextStyle by mutableStateOf(button1)
         private set
     var button2: TextStyle by mutableStateOf(button2)
         private set
     var button2B: TextStyle by mutableStateOf(button2B)
-        private set
-    var number1: TextStyle by mutableStateOf(number1)
-        private set
-    var number2: TextStyle by mutableStateOf(number2)
-        private set
-    var number3: TextStyle by mutableStateOf(number3)
         private set
     var keyword1: TextStyle by mutableStateOf(keyword1)
         private set
@@ -96,21 +109,25 @@ class RecordyTypography internal constructor(
         title1: TextStyle = this.title1,
         title2: TextStyle = this.title2,
         title3: TextStyle = this.title3,
+        title3SB: TextStyle = this.title3SB,
+        title4: TextStyle = this.title4,
         subtitle: TextStyle = this.subtitle,
+        emptybodyB: TextStyle = this.emptybodyB,
         body1M: TextStyle = this.body1M,
         body1R: TextStyle = this.body1R,
         body2M: TextStyle = this.body2M,
+        body2SB: TextStyle = this.body2SB,
         body2B: TextStyle = this.body2B,
         body2L: TextStyle = this.body2L,
-        caption1: TextStyle = this.caption1,
+        caption1R: TextStyle = this.caption1R,
+        caption1M: TextStyle = this.caption1M,
         caption1U: TextStyle = this.caption1U,
-        caption2: TextStyle = this.caption2,
+        caption1SB: TextStyle = this.caption1SB,
+        caption2R: TextStyle = this.caption2R,
+        caption2M: TextStyle = this.caption2M,
         button1: TextStyle = this.button1,
         button2: TextStyle = this.button2,
         button2B: TextStyle = this.button2B,
-        number1: TextStyle = this.number1,
-        number2: TextStyle = this.number2,
-        number3: TextStyle = this.number3,
         keyword1: TextStyle = this.keyword1,
         keyword2: TextStyle = this.keyword2,
         keyword3: TextStyle = this.keyword3,
@@ -120,21 +137,25 @@ class RecordyTypography internal constructor(
         title1,
         title2,
         title3,
+        title3SB,
+        title4,
         subtitle,
+        emptybodyB,
         body1M,
         body1R,
         body2M,
+        body2SB,
         body2B,
         body2L,
-        caption1,
+        caption1R,
+        caption1M,
         caption1U,
-        caption2,
+        caption1SB,
+        caption2R,
+        caption2M,
         button1,
         button2,
         button2B,
-        number1,
-        number2,
-        number3,
         keyword1,
         keyword2,
         keyword3,
@@ -146,21 +167,24 @@ class RecordyTypography internal constructor(
         title1 = other.title1
         title2 = other.title2
         title3 = other.title3
+        title3SB = other.title3SB
+        title4 = other.title4
         subtitle = other.subtitle
+        emptybodyB = other.emptybodyB
         body1M = other.body1M
         body1R = other.body1R
         body2M = other.body2M
         body2B = other.body2B
         body2L = other.body2L
-        caption1 = other.caption1
+        caption1R = other.caption1R
+        caption1M = other.caption1M
         caption1U = other.caption1U
-        caption2 = other.caption2
+        caption1SB = other.caption1SB
+        caption2R = other.caption2R
+        caption2M = other.caption2M
         button1 = other.button1
         button2 = other.button2
         button2B = other.button2B
-        number1 = other.number1
-        number2 = other.number2
-        number3 = other.number3
         keyword1 = other.keyword1
         keyword2 = other.keyword2
         keyword3 = other.keyword3
@@ -176,6 +200,7 @@ fun RecordyTypography(): RecordyTypography {
             fontSize = 26.sp,
             lineHeight = 38.sp,
             letterSpacing = (-0.5).sp,
+            color = ViskitYellow500
         ),
         title1 = TextStyle(
             fontFamily = PretendardBold,
@@ -193,10 +218,26 @@ fun RecordyTypography(): RecordyTypography {
             fontSize = 18.sp,
             lineHeight = 24.sp,
         ),
+        title3SB = TextStyle(
+            fontFamily = PretendardSemiBold,
+            fontSize = 18.sp,
+            lineHeight = 24.sp,
+        ),
+        title4 = TextStyle(
+            fontFamily = PretendardExtraBold,
+            fontSize = 17.sp,
+            lineHeight = 24.sp,
+        ),
         subtitle = TextStyle(
             fontFamily = PretendardBold,
             fontSize = 16.sp,
             lineHeight = 28.sp,
+        ),
+        emptybodyB = TextStyle(
+            fontFamily = PretendardBold,
+            fontSize = 20.sp,
+            lineHeight = 30.sp,
+            color = ViskitYellow500,
         ),
         body1M = TextStyle(
             fontFamily = PretendardMedium,
@@ -207,9 +248,16 @@ fun RecordyTypography(): RecordyTypography {
             fontFamily = PretendardRegular,
             fontSize = 16.sp,
             lineHeight = 24.sp,
+            color = ViskitYellow500,
         ),
         body2M = TextStyle(
             fontFamily = PretendardMedium,
+            fontSize = 14.sp,
+            lineHeight = 20.sp,
+            letterSpacing = (-0.5).sp,
+        ),
+        body2SB = TextStyle(
+            fontFamily = PretendardSemiBold,
             fontSize = 14.sp,
             lineHeight = 20.sp,
             letterSpacing = (-0.5).sp,
@@ -224,8 +272,14 @@ fun RecordyTypography(): RecordyTypography {
             fontFamily = PretendardMedium,
             fontSize = 14.sp,
             lineHeight = 24.sp,
+            letterSpacing = (-0.5).sp,
         ),
-        caption1 = TextStyle(
+        caption1R = TextStyle(
+            fontFamily = PretendardRegular,
+            fontSize = 12.sp,
+            lineHeight = 18.sp,
+        ),
+        caption1M = TextStyle(
             fontFamily = PretendardMedium,
             fontSize = 12.sp,
             lineHeight = 18.sp,
@@ -236,7 +290,17 @@ fun RecordyTypography(): RecordyTypography {
             lineHeight = 18.sp,
             textDecoration = TextDecoration.Underline,
         ),
-        caption2 = TextStyle(
+        caption1SB = TextStyle(
+            fontFamily = PretendardSemiBold,
+            fontSize = 12.sp,
+            lineHeight = 18.sp,
+        ),
+        caption2R = TextStyle(
+            fontFamily = PretendardRegular,
+            fontSize = 10.sp,
+            lineHeight = 18.sp,
+        ),
+        caption2M = TextStyle(
             fontFamily = PretendardMedium,
             fontSize = 10.sp,
             lineHeight = 18.sp,
@@ -255,21 +319,6 @@ fun RecordyTypography(): RecordyTypography {
             fontFamily = PretendardBold,
             fontSize = 14.sp,
             lineHeight = 18.sp,
-        ),
-        number1 = TextStyle(
-            fontFamily = CocogooseproThin,
-            fontSize = 42.sp,
-            lineHeight = 40.sp,
-        ),
-        number2 = TextStyle(
-            fontFamily = CocogooseproThin,
-            fontSize = 32.sp,
-            lineHeight = 40.sp,
-        ),
-        number3 = TextStyle(
-            fontFamily = CocogooseproThin,
-            fontSize = 20.sp,
-            lineHeight = 40.sp,
         ),
         keyword1 = TextStyle(
             fontFamily = PretendardMedium,

c193c38cfe371c4cb531be6f0fc3eb692afc2852
sangwook <operawook@gmail.com>
2024-09-25 19:10:17 +0900

    [add] #125 detail module

---
+++ feature/detail/src/main/java/com/record/detail/DetailScreen.kt
@@ -0,0 +1,10 @@
+package com.record.detail
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun DetailRoute(
+    modifier: Modifier = Modifier,
+) {
+}

e7f18c9e00a44c563a300cef1a6e93c1e3c8541c
윤서희 <appleid21@sookmyung.ac.kr>
2024-10-09 01:06:53 +0900

    [feat] #130 검색 결과 있을 때 component

---
+++ feature/search/src/main/java/com/record/search/component/ExhibitionTitle.kt
@@ -0,0 +1,29 @@
+package com.record.search.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.record.designsystem.theme.RecordyTheme
+
+@Composable
+fun ExhibitionTitle(
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .clip(RoundedCornerShape(8.dp))
+            .background(color = RecordyTheme.colors.gray10)
+            .fillMaxWidth()
+            .padding(vertical = 12.dp, horizontal = 16.dp)
+    ) {
+        Text(text = "전시회장", style = RecordyTheme.typography.caption1R, color = RecordyTheme.colors.gray03)
+    }
+}

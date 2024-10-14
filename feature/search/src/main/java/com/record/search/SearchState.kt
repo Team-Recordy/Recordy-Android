dd9db49ae22e43dfbf96c0bcfd1f3221e358185a
윤서희 <appleid21@sookmyung.ac.kr>
2024-10-14 19:29:04 +0900

    [chore] #130 ktlint format

--- feature/search/src/main/java/com/record/search/SearchState.kt
+++ feature/search/src/main/java/com/record/search/SearchState.kt
@@ -1,11 +1,11 @@
 package com.record.search
 
-import com.record.ui.base.UiState
 import com.record.ui.base.SideEffect
+import com.record.ui.base.UiState
 
 data class SearchState(
     val query: String = "",
-    val filteredItems: List<Exhibition> = emptyList()
+    val filteredItems: List<Exhibition> = emptyList(),
 ) : UiState
 
 sealed interface SearchSideEffect : SideEffect

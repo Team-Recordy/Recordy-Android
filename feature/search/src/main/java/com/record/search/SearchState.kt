dedd313434bba670fd0c22542b0425f46f234c77
윤서희 <appleid21@sookmyung.ac.kr>
2024-10-15 01:12:48 +0900

    [mod] #130 modify to immutable list

--- feature/search/src/main/java/com/record/search/SearchState.kt
+++ feature/search/src/main/java/com/record/search/SearchState.kt
@@ -2,10 +2,12 @@ package com.record.search
 
 import com.record.ui.base.SideEffect
 import com.record.ui.base.UiState
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.toImmutableList
 
 data class SearchState(
     val query: String = "",
-    val filteredItems: List<Exhibition> = emptyList(),
+    val filteredItems: ImmutableList<ExhibitionData> = emptyList<ExhibitionData>().toImmutableList(),
 ) : UiState
 
 sealed interface SearchSideEffect : SideEffect

b0febef941e4765a94e80da7e97dee31e6537bce
sangwook <operawook@gmail.com>
2024-10-26 21:25:26 +0900

    [feat] #140 home api connect

--- feature/home/build.gradle.kts
+++ feature/home/build.gradle.kts
@@ -9,6 +9,7 @@ android {
 dependencies {
     implementation(projects.domain.video)
     implementation(projects.domain.keyword)
+    implementation(projects.domain.exhibition)
     implementation(libs.lottie.compose)
     implementation(libs.collapsing.toolbar)
     implementation(libs.google.location)

16193152d40edb8eef7cc25b1de9b4d37266f0ba
윤서희 <appleid21@sookmyung.ac.kr>
2024-10-30 10:06:37 +0900

    [chore] #140 ktlint format

--- feature/home/src/main/java/com/record/home/HomeViewModel.kt
+++ feature/home/src/main/java/com/record/home/HomeViewModel.kt
@@ -28,7 +28,7 @@ class HomeViewModel @Inject constructor(
     }
 
     fun resetPlaces() = viewModelScope.launch {
-        exhibitionRepository.getNearPlaceData(0, if(uiState.value.exhibitionList.size < 10) 10 else uiState.value.exhibitionList.size, uiState.value.location.latitude, uiState.value.location.longitude)
+        exhibitionRepository.getNearPlaceData(0, if (uiState.value.exhibitionList.size < 10) 10 else uiState.value.exhibitionList.size, uiState.value.location.latitude, uiState.value.location.longitude)
             .onSuccess {
                 intent {
                     copy(exhibitionList = (it.data).toImmutableList())

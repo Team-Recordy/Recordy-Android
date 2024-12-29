e2bd472edb7c98770987e713e22b62d537e9fb99
윤서희 <appleid21@sookmyung.ac.kr>
2024-12-30 04:54:48 +0900

    [mod] #148 home ui

--- feature/detail/src/main/java/com/record/detail/screen/EmptyDataScreen.kt
+++ feature/detail/src/main/java/com/record/detail/screen/EmptyDataScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -37,17 +38,29 @@ fun EmptyDataScreen(
                 horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier.fillMaxSize(),
             ) {
+                val messages = message.split("\n")
                 Text(
-                    text = message,
+                    text = messages.getOrNull(0) ?: "",
                     style = RecordyTheme.typography.title3SB,
                     color = RecordyTheme.colors.gray02,
                     textAlign = TextAlign.Center,
                 )
+                if (messages.size > 1) {
+                    Spacer(modifier = Modifier.height(5.dp))
+                    Text(
+                        text = messages[1],
+                        style = RecordyTheme.typography.title3SB,
+                        color = RecordyTheme.colors.gray02,
+                        textAlign = TextAlign.Center,
+                    )
+                }
+
                 Spacer(modifier = Modifier.height(24.dp))
                 if (showButton) {
                     BasicButton(
                         modifier = Modifier
-                            .height(44.dp),
+                            .height(44.dp)
+                            .wrapContentWidth(),
                         text = "영상 업로드하기",
                         textStyle = RecordyTheme.typography.body2B,
                         textColor = RecordyTheme.colors.background,

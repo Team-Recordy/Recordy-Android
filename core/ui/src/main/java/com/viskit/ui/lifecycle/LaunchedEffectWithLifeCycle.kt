6e69e58b7eee87e4835e192a064af85d88f53bc8
cacaocoffee <cacaocoffee16@gmail.com>
2025-07-23 17:26:13 +0900

    [refactor] #168 update LocalLifecycleOwner import

--- core/ui/src/main/java/com/viskit/ui/lifecycle/LaunchedEffectWithLifeCycle.kt
+++ core/ui/src/main/java/com/viskit/ui/lifecycle/LaunchedEffectWithLifeCycle.kt
@@ -2,7 +2,7 @@ package com.viskit.ui.lifecycle
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.repeatOnLifecycle
